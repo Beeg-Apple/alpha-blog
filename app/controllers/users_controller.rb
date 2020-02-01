0386e763e3506ee8e0b14f88d1132e46cfebf9c8
Rob Medynski <robertmedynski@hotmail.com>
2020-02-01 14:44:04 +1100

    make set_user obsolete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,9 +22,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -34,17 +36,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
-
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
   def require_same_user
